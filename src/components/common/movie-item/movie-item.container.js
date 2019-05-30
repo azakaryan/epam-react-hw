9aec02bbc42d2652e3a119ad0e6fab275afddf1c
azakaryan <armenzakary@gmail.com>
2019-05-30 16:26:06 +0200

    Added reselect

--- src/components/common/movie-item/movie-item.container.js
+++ src/components/common/movie-item/movie-item.container.js
@@ -1,8 +1,17 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { MovieItem } from './movie-item';
 
+const moviesSelector = (state) => state.get('movies');
+const movieIdSelector = (state, movieId) => movieId;
+
+const getMovieById = createSelector(
+  [moviesSelector, movieIdSelector],
+  (movies, id) => movies.find(m => m.id === id),
+);
+
 const mapStateToProps = state => ({
-  getMovie: movieId => state.get('movies').find(m => m.id === movieId),
+  getMovie: movieId => getMovieById(state, movieId),
 });
 
 export default connect(mapStateToProps)(MovieItem);
